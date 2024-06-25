c87f796e79cbb46413008f9119d5a14edfcf3905
Deepanshu Baliyan <117852496+DeepanshuBaliyan@users.noreply.github.com>
2024-06-26 01:18:22 +0530

    Add files via upload

---
+++ PrintFromNToOne.java
@@ -0,0 +1,16 @@
+package BackTracking;
+
+public class PrintFromNToOne {
+    public static void func(int i, int n){
+        if(i<n){
+            return;
+        }
+        func(i+1, n);
+        System.out.println(i);
+
+    }
+    public static void main(String[] args) {
+        int n=4;
+        func(1, n);
+    }
+}
